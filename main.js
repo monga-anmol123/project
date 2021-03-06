020862d360f9f44f674e423e37a5f991d78e0b7e
monga-anmol123 <31888500+monga-anmol123@users.noreply.github.com>
2021-03-06 17:01:47 +0530

    Add files via upload
    3rd projet of js

--- main.js
+++ main.js
@@ -1,47 +1,83 @@
-const col = [ " green" , "blue" , "orange" , "yellow"]; 
-
-function selectone() {
-    let v =col[Math.floor(Math.random() * col.length)]
-    document.getElementById("target").style.backgroundColor = v ;
-    document.getElementById("change").innerText= v;
-}
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+// local reviews data
+const reviews = [
+  {
+    id: 0,
+    name: "susan smith",
+    job: "web developer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  } ,
+  {
+    id: 1,
+    name: "anna johnson",
+    job: "web designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 2,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 3,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   }
+];
 
-  function selectonehex() {
-    let v = getRandomColor();
-    document.getElementById("target").style.backgroundColor = v ;
-    document.getElementById("change").innerText= v;
-}
 
-function hexToRGB(h) {
-    let r = 0, g = 0, b = 0;
-  
-    // 3 digits
-    if (h.length == 4) {
-      r = "0x" + h[1] + h[1];
-      g = "0x" + h[2] + h[2];
-      b = "0x" + h[3] + h[3];
-  
-    // 6 digits
-    } else if (h.length == 7) {
-      r = "0x" + h[1] + h[2];
-      g = "0x" + h[3] + h[4];
-      b = "0x" + h[5] + h[6];
-    }
-    
-    return "RGB("+ +r + "," + +g + "," + +b + ")";
-  }
+let count=0;
 
-function selectonesimp() {
-    let v = getRandomColor();
-    let p = hexToRGB(v);    
-    document.getElementById("target").style.backgroundColor = v ;
-    document.getElementById("change").innerText= p;
-}
+const prev=document.getElementById('previous');
+const next= document.getElementById('next');
+
+const rand = document.getElementById("random");
+
+let job= document.getElementById("job");
+let info= document.getElementById("info");
+let photo= document.getElementById("photo");
+let author= document.getElementById("author");
+
+
+prev.onclick = function(){
+    count--;
+    if(count<0)
+    count=3;
+
+    job.textContent = reviews[count].job;
+    info.textContent = reviews[count].text;
+    author.textContent= reviews[count].name;
+    photo.src = reviews[count].img;
+};
+next.onclick = function(){
+    count++;
+    if(count>= reviews.length)
+    count=0;
+
+    job.textContent = reviews[count].job;
+    info.textContent = reviews[count].text;
+    author.textContent= reviews[count].name;
+    photo.src = reviews[count].img;
+};
+
+rand.onclick = function(){
+     count = Math.floor(Math.random() * reviews.length);
+
+    job.textContent = reviews[count].job;
+    info.textContent = reviews[count].text;
+    author.textContent= reviews[count].name;
+    photo.src = reviews[count].img;
+};
